ba4184255a83e6e178a879030a246dfb34ad6866
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-04-17 15:07:47 +0200

    Creo funzione che genera la creazione delle card

--- js/script.js
+++ js/script.js
@@ -3,6 +3,23 @@ const ApiURL = "https://lanciweb.github.io/demo/api/pictures/";
 // Selezione dell'elemento HTML in cui inserire i dati recuperati
 const photoListElement = document.getElementById("photo-list");
 
+// Funzione per creare una card
+function createCard(photo) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    card.innerHTML = `
+        <img class="photo-pin" src="/img/pin.svg" alt="puntina">
+        <div class="photo-image">
+            <img src="${photo.url}" alt="${photo.title}">
+        </div>
+        <div class="photo-date">${photo.date}</div>
+        <h2 class="photo-title">${photo.title}</h2>
+    `;
+
+    return card;
+}
+
 // Esempio di chiamata fetch per recuperare i dati dall'API
 fetch(ApiURL)
     .then((response) => {
